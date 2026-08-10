f3c7b85f348c9e870fa5a57473a0bb1403898a91
Jithendar das Nethala <jithendardas20@gmail.com>
2026-08-10 16:05:52 +0530

    Time: 2 ms (92.05%) | Memory: 47.8 MB (38.79%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.java
@@ -0,0 +1,26 @@
+import java.util.*; 
+
+class Solution {
+    public void moveZeroes(int[] arr) {
+        int n= arr.length;
+        
+        int count = 0;
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i] != 0)
+            {
+                arr[count++]=arr[i];
+            }
+        }
+        while(count<n)
+        {
+            arr[count++]=0;
+        }
+
+    //     for(int i=0;i<n;i++)
+    //     {
+    //         System.out.print(arr[i]+" ");
+    // }
+
+    }
+}
